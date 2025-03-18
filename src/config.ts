c30b2f55185dcbe258219a31156fc589984e6abf
Harsh Gujarathi <gujarathiharsh02@gmail.com>
2025-03-18 21:54:42 +0530

    update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Harsh Gujarathi';
-export const SITE_DESCRIPTION = 'This is the personal website/blog page of Harsh Gujarathi.';
+export const SITE_DESCRIPTION = 'Hey! Welcome to my personal website';
